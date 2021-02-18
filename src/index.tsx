ed3ee3bd953714ce6db65b8ac064bf54f0c1632f
Franklin Liu <FLIU253@uwo.ca>
2021-02-18 13:55:34 -0500

    allowing code editing in frame

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import {fetchPlugin} from './plugins/fetch-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
@@ -24,21 +25,42 @@ const App = () => {
       return;
     }
 
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin() ,fetchPlugin(input)],
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
-          'process.env.NODE_ENV': '"production"',
-          global: 'window'
-      }
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
     });
 
-    // console.log(result);
-
     setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+    <html>
+        <head></head>
+        <body>
+            <div id="root"></div>
+            <script>
+                window.addEventListener('message', (event) => {
+                    try{
+                        eval(event.data);
+                    }catch(err){
+                        const root = document.querySelector('#root');
+                        root.innerHTML = '<div style= "color: red;"> <h4>Runtime Error</h4>' + err + '</div>'
+                        console.log(err);
+                    }
+                }, false);
+            </script>
+        </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -49,7 +71,9 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      {/* <pre>{code}</pre> */}
+      {/* eslint-disable-next-line jsx-a11y/iframe-has-title */}
+      <iframe ref = {iframe} sandbox="allow-scripts" srcDoc={html} />
     </div>
   );
 };
