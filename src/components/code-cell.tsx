9dbfcb6ab435fd76ec122883ec05ddc35f199f9f
Franklin Liu <FLIU253@uwo.ca>
2021-02-18 22:41:45 -0500

    Added more error handling

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,19 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
 import bundle from '../bundler';
 import Resizable from './resizable';
-import { ResizableBox } from 'react-resizable';
 
 const CodeCell = () => {
   const [code, setCode] = useState('');
+  const [err, setErr] = useState('');
   const [input, setInput] = useState('');
 
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output.code);
+      setErr(output.err);
+    }, 1000);
 
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [input]);
 
   return (
     <Resizable direction="vertical">
@@ -21,7 +27,7 @@ const CodeCell = () => {
         <Resizable direction="horizontal">
           <CodeEditor initialValue = "const a = 1;" onChange = {(value) => setInput(value)}/>
         </Resizable>
-      <Preview code = {code} />
+      <Preview code = {code} err = {err} />
     </div>
     </Resizable>
   );
